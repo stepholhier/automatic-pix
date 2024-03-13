0555f07fafc402db8c2aad100d60486abfef471b
Stephano <stegoodds@gmail.com>
2024-03-12 22:33:46 -0300

    Updating app.py & Implementing payment.py & database.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, jsonify
+from repository.database import db
+from db_models.payment import Payment
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
+
+
+db.init_app(app)
 
 # Criando Pagamento
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
     return jsonify({"message": "The payment has been created"})
-
+# Confirmando Pagamento
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
     return jsonify({"message": "The payment has been confirmed"})
