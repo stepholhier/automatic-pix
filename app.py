4bdb1472a5cfa4a098feb92106b19613a6e1ba53
Stephano <stegoodds@gmail.com>
2024-03-14 22:32:29 -0300

    Updating app.py & implemeting new features

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from repository.database import db
 from db_models.payment import Payment
+from datetime import datetime, timedelta
 
 
 app = Flask(__name__)
@@ -13,12 +14,28 @@ db.init_app(app)
 # Criando Pagamento
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
-    return jsonify({"message": "The payment has been created"})
+    data = request.get_json()
+
+    if 'value'not in data:
+        return jsonify({"message": "Invalid value"}), 400
+    
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data['value'], 
+                          expiration_date=expiration_date)
+    
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify({"message": "The payment has been created", 
+                    "payment": new_payment.to_dict()})
+
+
 # Confirmando Pagamento
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
     return jsonify({"message": "The payment has been confirmed"})
-
+# Visualização de pagamento existente
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
     return 'pagamento pix'
